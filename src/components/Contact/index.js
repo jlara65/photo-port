a3d49a9017f8ecdfba87493f62d1d345cdedb9e1
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-11-21 22:40:08 -0600

    Created Contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,30 @@
+// CONTACT INDEX.JS!!!
+import React from 'react';
+
+function ContactForm() {
+  return (
+    <section>
+      <h1>Contact me</h1>
+      <form id="contact-form">
+        {/* name input */}
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
+        </div>
+        {/* email input */}
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" />
+        </div>
+        {/* message text area */}
+        <div>
+          <label htmlFor="message">Message:</label>
+          <input name="message" row="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
